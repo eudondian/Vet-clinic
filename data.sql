ce77e2e20faa5de6a0a8a0cdec09725fbad3c8df
eudondian <eudondian@gmail.com>
2023-06-09 01:44:55 +0100

    Updated data.sql file

--- data.sql
+++ data.sql
@@ -58,3 +58,47 @@ CASE
 END
 );	
 
+/* Multiple tables */
+
+INSERT INTO vets ( name, age, date_of_graduation)
+VALUES
+	( 'Vet William Tatcher', 45, '2000-04-23'),
+	( 'Vet Maisy Smith', 26, '2019-01-17'),
+	( 'Vet Stephanie Mendez', 64, '1981-05-04'),
+	( 'Vet Jack Harkness', 38, '2008-06-08');
+
+  /* Data for Spacialties*/
+
+INSERT INTO specializations (vets_id, species_id)
+VALUES
+	(1, 1),
+	(3, 2),
+	(3, 1),
+	(4, 2);
+	
+/* Data for visits*/
+
+INSERT INTO visits (animals_id, vets_id, date_of_visit)
+VALUES
+	(1, 1, '2020-05-24'),
+	(1, 3, '2020-07-22'),
+	(2, 4, '2021-02-02'),
+	(7, 2, '2020-01-05'),
+	(7, 2, '2020-03-08'),
+	(7, 2, '2020-05-14'),
+	(3, 3, '2021-05-04'),
+	(8, 4, '2021-02-24'),
+	(4, 2, '2019-12-21'),
+	(4, 1, '2020-08-10'),
+	(4, 2, '2021-04-07'),
+	(9, 3, '2019-09-29'),
+	(5, 4, '2020-10-03'), 
+	(5, 4, '2020-11-04'),
+	(6, 2, '2019-01-24'),
+	(6, 2, '2019-05-15'),
+	(6, 2, '2020-02-27'),
+	(6, 2, '2020-08-03'),
+	(10, 3, '2020-05-24'),
+	(10, 1, '2021-01-11');
+	
+	
